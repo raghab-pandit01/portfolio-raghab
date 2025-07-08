51bb33dde383b8cf70e81db3770d41ee9ae92099
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2025-07-09 00:06:05 +0545

    Refactor portfolio layout and add Home and Skills pages

---
+++ src/Pages/SkillsPage/SkillsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SkillsPage = () => {
+  return (
+    <>
+        <div className='bg-gradient-to-b from-[#274c77] to-[#6096ba] h-[calc(100vh-80px)]'>
+            
+        </div>
+    </>
+  )
+}
+
+export default SkillsPage
